f55f03af9767a8dacad64d21696ad3c1da37075b
Sana Khuram <126725476+sanakhuram@users.noreply.github.com>
2024-04-22 00:54:03 +0200

    .

---
+++ js/success.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    function displayMessage() {
+      const messageContainer = document.querySelector(".message-container");
+      messageContainer.style.display = "block";
+    }
+  
+    setTimeout(displayMessage, 1000);
+  });
